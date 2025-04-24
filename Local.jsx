97ca2cb85beb417efa812293754da746db111cb2
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-24 08:41:36 +0600

    add typesctipt in my project

--- Local.jsx
+++
@@ -1,7 +0,0 @@
-  apiKey: "AIzaSyCAhjL2CS87viZy8t94NCdEWFa_XqEPnHw",
-  authDomain: "techno-bloger.firebaseapp.com",
-  projectId: "techno-bloger",
-  storageBucket: "techno-bloger.firebasestorage.app",
-  messagingSenderId: "558123680725",
-  appId: "1:558123680725:web:25d9b30d3600fa454e335c"
-
